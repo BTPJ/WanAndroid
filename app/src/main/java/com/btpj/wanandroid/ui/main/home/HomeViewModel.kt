89ae496c11f2f7f62026112db678ff4f37825b6d
BTPJ <1069113473@qq.com>
2022-04-14 11:45:36 +0800

    处理没网崩溃的bug

--- app/src/main/java/com/btpj/wanandroid/ui/main/home/HomeViewModel.kt
+++ app/src/main/java/com/btpj/wanandroid/ui/main/home/HomeViewModel.kt
@@ -2,7 +2,6 @@ package com.btpj.wanandroid.ui.main.home
 
 import androidx.lifecycle.MutableLiveData
 import com.btpj.lib_base.base.BaseViewModel
-import com.btpj.lib_base.bean.ApiResponse
 import com.btpj.lib_base.bean.PageResponse
 import com.btpj.lib_base.ext.handleResponse
 import com.btpj.lib_base.ext.launch
@@ -10,7 +9,9 @@ import com.btpj.lib_base.utils.LogUtil
 import com.btpj.wanandroid.data.DataRepository
 import com.btpj.wanandroid.data.bean.Article
 import com.btpj.wanandroid.data.bean.Banner
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
 
 /**
  * 首页ViewModel
@@ -50,17 +51,24 @@ class HomeViewModel : BaseViewModel() {
     fun fetchArticlePageList(pageNo: Int = 0) {
         launch({
             if (pageNo == 0) {
-                // 第一页会同时请求置顶文章列表接口和分页文章列表的接口，使用async进行并行请求速度更快（默认是串行的）
-                val response1 = async { DataRepository.getArticlePageList(pageNo, PAGE_SIZE) }
-                val response2 = async { DataRepository.getArticleTopList() }
+                // 使用async需要单独加作用域,不然没网时会崩溃
+                withContext(Dispatchers.IO) {
+                    // 第一页会同时请求置顶文章列表接口和分页文章列表的接口，使用async进行并行请求速度更快（默认是串行的）
+                    val response1 = async { DataRepository.getArticlePageList(pageNo, PAGE_SIZE) }
+                    val response2 = async { DataRepository.getArticleTopList() }
 
-                handleResponse(response1.await(), {
-                    val list = response1.await()
-                    handleResponse(response2.await(), {
-                        (list.data.datas as ArrayList<Article>).addAll(0, response2.await().data)
-                        articlePageListLiveData.value = list.data
+                    handleResponse(response1.await(), {
+                        val list = response1.await()
+                        handleResponse(response2.await(), {
+                            (list.data.datas as ArrayList<Article>).addAll(
+                                0,
+                                response2.await().data
+                            )
+                            // 加了Dispatchers.IO现在是子线程,需要使用postValue的方式
+                            articlePageListLiveData.postValue(list.data)
+                        })
                     })
-                })
+                }
             } else {
                 val response = DataRepository.getArticlePageList(pageNo, PAGE_SIZE)
                 handleResponse(response, { articlePageListLiveData.value = response.data })

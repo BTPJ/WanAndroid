48720294e7aae616c42f38bdd0d550486d68dd0c
BTPJ <1069113473@qq.com>
2022-04-22 17:44:48 +0800

    完成切换IP的功能

--- app/src/main/java/com/btpj/wanandroid/base/BaseActivity.kt
+++ app/src/main/java/com/btpj/wanandroid/base/BaseActivity.kt
@@ -18,7 +18,9 @@ abstract class BaseActivity<VM : BaseViewModel, B : ViewDataBinding>(contentView
     override fun createObserve() {
         super.createObserve()
         mViewModel.errorResponse.observe(this) {
-            if (it?.errorCode == -1001) { // 需要登录
+            if (it?.errorCode == -1001) {
+                // 需要登录，这里主要是针对收藏操作，不想每个地方都判断一下
+                // 当然你也可以封装一个收藏的组件，在里面统一判断跳转
                 LoginActivity.launch(this)
             }
         }

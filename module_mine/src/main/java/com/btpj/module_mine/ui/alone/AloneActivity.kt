7a3e374a41d7ff2aecafe2ae39547bc9b982d53c
BTPJ <1069113473@qq.com>
2022-05-11 13:58:25 +0800

    优化代码

--- module_mine/src/main/java/com/btpj/module_mine/ui/alone/AloneActivity.kt
+++ module_mine/src/main/java/com/btpj/module_mine/ui/alone/AloneActivity.kt
@@ -1,18 +1,16 @@
 package com.btpj.module_mine.ui.alone
 
 import android.os.Bundle
-import com.alibaba.android.arouter.facade.annotation.Route
-import com.btpj.lib_base.base.BaseVMBActivity
-import com.btpj.lib_base.export.ModuleMineApi
 import com.btpj.lib_base.R
+import com.btpj.lib_base.base.BaseVMBActivity
 import com.btpj.lib_base.databinding.CommonContainerBinding
+import com.btpj.lib_base.export.ModuleMineApi
 
 /**
  * 作为Application时的测试页面
  *
  * @author LTP 2022/4/8
  */
-@Route(path = ModuleMineApi.ROUTER_MINE_LOGIN_ACTIVITY)
 class AloneActivity :
     BaseVMBActivity<AloneViewModel, CommonContainerBinding>(R.layout.common_container) {
 
--- module_wechat/src/main/java/com/btpj/module_wechat/ui/alone/AloneActivity.kt
+++ module_wechat/src/main/java/com/btpj/module_wechat/ui/alone/AloneActivity.kt
@@ -1,10 +1,8 @@
 package com.btpj.module_wechat.ui.alone
 
 import android.os.Bundle
-import com.alibaba.android.arouter.facade.annotation.Route
-import com.btpj.lib_base.base.BaseVMBActivity
-import com.btpj.lib_base.export.ModuleMineApi
 import com.btpj.lib_base.R
+import com.btpj.lib_base.base.BaseVMBActivity
 import com.btpj.lib_base.databinding.CommonContainerBinding
 import com.btpj.lib_base.export.ModuleWechatApi
 
@@ -13,7 +11,6 @@ import com.btpj.lib_base.export.ModuleWechatApi
  *
  * @author LTP 2022/4/8
  */
-@Route(path = ModuleMineApi.ROUTER_MINE_LOGIN_ACTIVITY)
 class AloneActivity :
     BaseVMBActivity<AloneViewModel, CommonContainerBinding>(R.layout.common_container) {
 

6e9b85cabd9616043bbc55067b967696c725f819
LTP <1069113473@qq.com>
2024-01-02 18:39:33 +0800

    优化主题色

--- app/src/main/java/com/btpj/wanandroid/ui/main/MainPage.kt
+++ app/src/main/java/com/btpj/wanandroid/ui/main/MainPage.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp

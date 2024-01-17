f3a3ffbbbfd7ec8a606dbee1dd0140860a906bf9
LTP <1069113473@qq.com>
2024-01-17 11:45:16 +0800

    优化标题，完成部分webView

--- app/src/main/java/com/btpj/wanandroid/ui/main/home/HomeViewModel.kt
+++ app/src/main/java/com/btpj/wanandroid/ui/main/home/HomeViewModel.kt
@@ -10,6 +10,7 @@ import com.btpj.wanandroid.data.DataRepository
 import com.btpj.wanandroid.data.bean.Article
 import com.btpj.wanandroid.data.bean.Banner
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -52,27 +53,33 @@ class HomeViewModel : BaseViewModel() {
         launch({
             if (pageNo == 0) {
                 // 第一页会同时请求置顶文章列表接口和分页文章列表的接口，使用async进行并行请求速度更快（默认是串行的）
+                // 需要加SupervisorJob()来自行处理协程（https://juejin.cn/post/7130132604568731655）
                 val job1 =
-                    async(Dispatchers.IO) {
+                    async(Dispatchers.IO + SupervisorJob()) {
                         DataRepository.getArticlePageList(
                             pageNo,
                             PAGE_SIZE
                         )
                     }
-                val job2 = async(Dispatchers.IO) { DataRepository.getArticleTopList() }
+                val job2 =
+                    async(Dispatchers.IO + SupervisorJob()) { DataRepository.getArticleTopList() }
 
-                val response1 = job1.await()
-                val response2 = job2.await()
+                try {
+                    val response1 = job1.await()
+                    val response2 = job2.await()
 
-                handleRequest(response1, {
-                    handleRequest(response2, {
-                        (response1.data.datas as ArrayList<Article>).addAll(
-                            0,
-                            response2.data
-                        )
-                        articlePageListLiveData.value = response1.data
+                    handleRequest(response1, {
+                        handleRequest(response2, {
+                            (response1.data.datas as ArrayList<Article>).addAll(
+                                0,
+                                response2.data
+                            )
+                            articlePageListLiveData.value = response1.data
+                        })
                     })
-                })
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             } else {
                 handleRequest(
                     DataRepository.getArticlePageList(pageNo, PAGE_SIZE),
